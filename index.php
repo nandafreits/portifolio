c6d6bd9e9b816c8e0edbb0a6f4c259e639f91207
nandafreits <mfernandsf@gmail.com>
2025-02-13 20:20:37 -0300

    aula13-02

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portifólio</title>
+
+    <style>
+        *{
+            margin:0;
+            padding: 0 ;
+            
+        }
+
+        p {
+            border: 1px solid pink 
+
+        }
+
+        h1 {
+            background: pink;
+
+        }
+        body{
+            background-image: url('imagens/logo.png');
+            background-repeat: no-repeat;
+            background-position: center;
+
+
+
+        }
+        </style>
+
+    <link rel="stylesheet" href="css/styles.css />
+    
+</head>
+<body>
+
+
+  <div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+
+            <div class="logo">
+
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
+
+            <ol>
+
+
+            </ol>
+
+        </nav>
+
+        <div class="title">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluçoes em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos ex voluptas fugiat. Praesentium, assumenda nemo?</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Linkedin</a>
+            <a href="#">Instagram</a>
+        </div>
+
+    </header>
+
+    
+
+  </div>
+
+
+</body>
+</html>
