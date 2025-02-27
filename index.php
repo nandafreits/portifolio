7e55a11c180cfbfea4ce018cfe044b6712e147fe
nandafreits <mfernandsf@gmail.com>
2025-02-27 20:33:00 -0300

    aula 27/02

--- index.php
+++ index.php
@@ -1,55 +1,60 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portifólio</title>
-    
-    <link rel="stylesheet" href="css/style.css" />
+
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
+
 <body>
 
 <div id="interface">
 
     <header class="cabecalho">
 
-        <nav class="menu">
+             <nav class="menu">
 
-            <div class="logo">
-                <img src="imagens/logo.png" alt="LOGO" width="150" />
-            </div>
+                <div class="logo">
 
-            <ul>
-                <li>Home</li>
-                <li>Serviços</li>
-                <li>Produtos</li>
-                <li>Portifólio</li>
-                <li>Sobre</li>
-                <li>Contato</li>
-            </ul>
+                    <img src="imagens/logo.png" alt="LOGO" width="150"/>
+
+</div>
+
+        <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Portifólio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contatos</a></li>
+        </ul>
 
         </nav>
 
         <div class="titles">
 
-            <h1 class="titulo">Entregando serviços de qualidade</h1>
-            <h1 class="subtitulo">Soluções em TI</h1>
+            <h1 class="título">Entregando serviços de qualidade</h1>
+            <h1 class="subtítulo">Soluções em TI</h1>
 
-            <p class="paragrafo">Ave Maria, gratia plena, Dominus tecum: benedicta tu in mulieribus et benedictus fructus ventris tui Iesu. Sancta Maria, Mater Dei, ora pro nobis peccatoribus, nunc et in hora mortis nostrae.Amen.</p>
-
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore doloribus ducimus quam vero provident a?</p>
         </div>
 
         <div class="socialIcons">
+
             <a href="#">Twitter</a>
             <a href="#">Facebook</a>
-            <a href="#">LinkedIn</a>
+            <a href="#">Linkedin</a>
             <a href="#">Instagram</a>
+    
         </div>
 
     </header>
 
 </div>
-    
+
 </body>
+
 </html>
