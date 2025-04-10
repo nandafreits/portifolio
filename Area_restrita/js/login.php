998d6c1b800b6b2b7a798fcf6d3319f0d7a0e524
nandafreits <mfernandsf@gmail.com>
2025-04-10 20:37:17 -0300

    aula10/04/2025

---
+++ Area_restrita/js/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Área Restrita - login</title>
+
+    <link rel="stylesheet" href="css/login.css" />
+</head>
+<body>
+    <div id="interface">
+        <form action="" method='POST'>
+            <h3 class="title-login">Digite o usuário e a senha</h3>
+
+            <input type="email" name="txt_email" id='txt_email' class='' placeholder="Digite seu email"/>
+            <input type="password" name="txt_pw" id="txt-pw" class="" placeholder="Digite sua senha" />
+
+            <div class="container-buttons">
+            <a href="../">Cancelar</a>
+            <button type="submit" name="bt-submit" id="bt-submit" class="">Entrar</button>
+            </div>
+
+        </form>
+    </div>
+</body>
+</html>
